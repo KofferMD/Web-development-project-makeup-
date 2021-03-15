e7be41619e7a87ce7391e74042d6d8f8166c4f5a
Nikolay <n_vinok98@mail.ru>
2021-03-15 10:42:27 +0300

    price

---
+++ main.js
@@ -0,0 +1,41 @@
+const anchors = document.querySelectorAll('a[href*="#anchor"]');
+
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function (event) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href');
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const tabs = document.querySelector('.tabs');
+    const tabsBtn = document.querySelectorAll('.tabs__btn');
+    const tabsContent = document.querySelectorAll('.tabs__content');
+
+
+
+    if (tabs) {
+        tabs.addEventListener('click', function (e) {
+            if (e.target.classList.contains('tabs__btn')) {
+                const tabsPath = e.target.dataset.tabsPath;
+                tabsHandler(tabsPath);
+            }
+        });
+    }
+
+    const tabsHandler = function (path) {
+        tabsBtn.forEach(function (el) {
+            el.classList.remove('tabs__btn--active');
+        });
+        document.querySelector(`[data-tabs-path="${path}"]`).classList.add('tabs__btn--active');
+
+        tabsContent.forEach(function (el) {
+            el.classList.remove('tabs__content--active');
+        });
+        document.querySelector(`[data-tabs-target="${path}"]`).classList.add('tabs__content--active');
+    };
+});
