a61e89087bf17d9f325bd1c23b68805543b1c2c1
Nikolay <n_vinok98@mail.ru>
2021-03-16 18:42:58 +0300

    END

--- js/main.js
+++ js/main.js
@@ -46,30 +46,34 @@ $(function () {
     $('.team__slider').slick({
         arrows: false,
         dots: true,
-        autoplay: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: false,
         responsive: [{
-                breakpoint: 1170,
+                breakpoint: 1164,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
                     infinite: true,
                     dots: true
                 }
             },
             {
-                breakpoint: 750,
+                breakpoint: 1024,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 2
                 }
             },
             {
-                breakpoint: 480,
+                breakpoint: 775,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1
                 }
-            }
+            },
+
+
         ]
     });
 
